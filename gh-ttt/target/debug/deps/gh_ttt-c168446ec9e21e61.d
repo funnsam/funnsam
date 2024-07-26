1de921e1354d46557eb5add67510274acc8a0a06
funnsam <funnsam8@gmail.com>
2024-07-26 19:18:56 +0800

    yes gamez

---
+++ gh-ttt/target/debug/deps/gh_ttt-c168446ec9e21e61.d
@@ -0,0 +1,5 @@
+/home/funnsam/Projects/personal/funnsam/gh-ttt/target/debug/deps/libgh_ttt-c168446ec9e21e61.rmeta: src/main.rs
+
+/home/funnsam/Projects/personal/funnsam/gh-ttt/target/debug/deps/gh_ttt-c168446ec9e21e61.d: src/main.rs
+
+src/main.rs:
